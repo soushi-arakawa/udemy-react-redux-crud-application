0ece5a0cdba8353d040be361bb36f7339086391d
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-04 07:02:36 +0900

    add h1 tag to Welcome message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 
 function App() {
-  return <div><p>Hello, world!</p></div>;
+  return <div><h1>Hello, world!</h1></div>;
 }
+
+// import React from 'react'
+
+// function App() {
+//   return React.createElement("div", null, "Hello, world!!")
+// }
 
 export default App;
