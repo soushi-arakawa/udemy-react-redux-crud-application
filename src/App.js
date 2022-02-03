190aa3ebe17f125dd774148e4adfe35fb22bbcf1
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-04 07:14:33 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
 
 function App() {
-  return <div><h1>Hello, world!</h1></div>;
+  // const greeting = "Hi, Tom!"
+  // const dom = <h1 className="foo">{greeting}</h1>
+  // return dom
+
+  // return <input type="text" onClick={() => {console.log("I am clicked!")}} />
+
+  return (
+    // <div>
+    //   <label htmlFor="bar">bar</label>
+    //   <input type="text" onChange={() => {console.log("I am clicked!")}} />
+    // </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => { console.log("I am clicked!") }} />
+    </React.Fragment>
+  )
 }
-
-// import React from 'react'
-
-// function App() {
-//   return React.createElement("div", null, "Hello, world!!")
-// }
 
 export default App;
